e44b3ee49e9dea844c7b96153a5c3ae8a134477e
reyes-diego <424135339@pcpuma.acatlan.unam.mx>
2025-09-14 18:17:28 -0600

    Arreglo_recorrido.c

--- data_structure/homework/programs/Arreglo_recorrido.c
+++ data_structure/homework/programs/Arreglo_recorrido.c
@@ -1,19 +1,19 @@
 /*Programa: P2E - Recorrido de matriz bidimensional
 
 
-Realice el programa que dada una matriz de m*n, en donde el usuario introducir· el valor de m y n, 
-siendo estos m·ximos 10, el arreglo es del tipo entero. Realice el siguiente recorrido:
+Realice el programa que dada una matriz de m*n, en donde el usuario introducir√° el valor de m y n, 
+siendo estos m√°ximos 10, el arreglo es del tipo entero. Realice el siguiente recorrido:
 
 1. Recorrido por fila comenzando desde la casilla 00, siguiente 01, 02, 03... luego 10, 11... hasta mn.
 
 2. Recorrido por columnas comenzando desde mn , n-1mm ,n-2m , hasta 0m, para continuar con 
-   nm-1,n-1m-1,n-2m...0m-1.. el proceso deber· continuar hasta llegar a la casilla 00
+   nm-1,n-1m-1,n-2m...0m-1.. el proceso deber√° continuar hasta llegar a la casilla 00
 
 3. Realice el recorrido en diagonal desde la casilla 00, 11, 22, 33, siempre que n = m.
 
 //424135339 REYES GONZALEZ DIEGO DE JESUS 10/09/25 
 
-//NOTA TODOS LOS PROGRAMAS DEBEN ESTAR ESCRITOS EN INGL…S Y USAR ARGUMENTOS SI SE REQUIERE EN LAS INSTRUCCIONES.
+//NOTA TODOS LOS PROGRAMAS DEBEN ESTAR ESCRITOS EN INGL√âS Y USAR ARGUMENTOS SI SE REQUIERE EN LAS INSTRUCCIONES.
 */
 
 #include <stdlib.h>
